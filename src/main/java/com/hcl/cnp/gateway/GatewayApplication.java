5d8f0f9e355bdd2dd3750587f92c1791000348f1
Pepe <pboucourt@tmunited.co>
2018-07-22 22:58:20 -0400

    Working on create routes from discovery server

--- src/main/java/com/hcl/cnp/gateway/GatewayApplication.java
+++ src/main/java/com/hcl/cnp/gateway/GatewayApplication.java
@@ -5,11 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
-;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -17,21 +14,6 @@ public class GatewayApplication {
 	@Bean
 	DiscoveryClientRouteDefinitionLocator discoveryRoutes(DiscoveryClient dc, DiscoveryLocatorProperties dlp) {
 		return new DiscoveryClientRouteDefinitionLocator(dc, dlp);
-	}
-
-	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route(p -> p
-						.path("/patient-service/delay")
-						.filters(f -> f.addRequestHeader("Hello", "World"))
-						.uri("http://httpbin.org:80")
-				)
-				.route(p -> p
-						.host("*.hystrix.com")
-						.filters(f -> f.hystrix(config -> config.setName("mycmd")))
-						.uri("http://httpbin.org:80"))
-				.build();
 	}
 
 	public static void main(String[] args) {
