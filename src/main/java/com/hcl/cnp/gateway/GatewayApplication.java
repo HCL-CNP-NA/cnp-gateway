ddc3650845954d30fd86fe0338640f8e25ef6495
Pepe <pboucourt@tmunited.co>
2018-07-25 23:04:05 -0400

    routing patient-service rests

--- src/main/java/com/hcl/cnp/gateway/GatewayApplication.java
+++ src/main/java/com/hcl/cnp/gateway/GatewayApplication.java
@@ -2,18 +2,38 @@ package com.hcl.cnp.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
-import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 
 
 @SpringBootApplication
 public class GatewayApplication {
 
+//	@Bean
+//	RouterFunction<?> routes() {
+//		return
+//				route(GET("/delay"), r -> ok().body(Flux.just("Hello HCL!").delayElements(Duration.ofSeconds(10)), String.class));
+//	}
+
 	@Bean
-	DiscoveryClientRouteDefinitionLocator discoveryRoutes(DiscoveryClient dc, DiscoveryLocatorProperties dlp) {
-		return new DiscoveryClientRouteDefinitionLocator(dc, dlp);
+	RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("delay", r -> r.path("/patient-service/delay")
+						.uri("http://localhost:8080/delay"))
+				.route("findAll", r -> r.path("/patient-service/find-all")
+						.uri("http://localhost:8080/find-all"))
+//				.route("findById", r -> r.path("/patient-service/patient/**")
+//						.filters(f -> f.filter((exchange, filterChain) -> {
+//							String id = exchange.getRequest().getQueryParams().getFirst("id");
+//							ServerHttpRequest request = exchange.getRequest().mutate()
+//									.path("/patient-service/patient/"+id)
+//									.build();
+//							return filterChain.filter(exchange.mutate().request(request).build());
+//						}))
+//						.uri("http://localhost:8080/patient"))
+				.build();
 	}
 
 	public static void main(String[] args) {
