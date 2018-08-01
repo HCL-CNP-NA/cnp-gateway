a1438a0d76a4e9c7a4739670875126cca51f26d5
Pepe <pboucourt@tmunited.co>
2018-08-01 16:33:53 -0400

    Updating dependencies and routes

--- src/main/java/com/hcl/cnp/gateway/GatewayApplication.java
+++ src/main/java/com/hcl/cnp/gateway/GatewayApplication.java
@@ -7,7 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GatewayApplication {
 
+//	@Bean
+//	DiscoveryClientRouteDefinitionLocator discoveryRoutes(DiscoveryClient dc, DiscoveryLocatorProperties dlp) {
+//		return new DiscoveryClientRouteDefinitionLocator(dc, dlp);
+//	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
+
 }
